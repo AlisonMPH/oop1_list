64c13399c952fcfdfe8fd0ec7d2128de8bda6881
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-03-05 14:38:06 -0300

    [ADDED] Overcharged Coonstructor

--- question3/src/com/san/Car/Car.java
+++ question3/src/com/san/Car/Car.java
@@ -5,17 +5,31 @@ import com.san.Vehicle.Vehicle;
 public abstract class Car extends Vehicle  {
     protected short numberOfDoors;
     protected double trunkSize;
+    protected float trunkBedSize;
     
     public Car(
         String name, 
         String brand, 
         String color, 
         int year,
-        float mileage, 
+        double mileage, 
         double trunkSize
     ) {
         super (name, brand, color, year, mileage);
         this.trunkSize = trunkSize;
+    }
+
+    public Car (
+        String name, 
+        String brand, 
+        String color, 
+        int year, 
+        double mileage,
+        float trunkBedSize
+    ) {
+        super (name, brand, color, year, mileage);
+        this.trunkBedSize = trunkBedSize;
+
     }
 
     public short getNumberOfDoors() {
