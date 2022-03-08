4d1477c27aa1af41e05a81d9ad4d6462f8a7c2a3
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-03-08 20:35:47 -0300

    [MODIFIED] Rename Option Menu

--- question3/src/com/san/Menu/ShowMenu.java
+++ question3/src/com/san/Menu/ShowMenu.java
@@ -4,7 +4,7 @@ public abstract class ShowMenu {
     public static void show() {
         ClearScreenMenu.clearScreen();
         System.out.format("%s\n\n", "-".repeat(Menu.QUANTITY_OF_CHARACTERS));
-        System.out.println("\t1 - Buy Vehicles");
+        System.out.println("\t1 - Vehicles for Sale");
         System.out.println("\t2 - Sell Vehicle");
         System.out.println("\t0 - Exit");
         System.out.format("\n%s\n\n", "-".repeat(Menu.QUANTITY_OF_CHARACTERS));
